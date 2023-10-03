883d7dfd3f4e9a2f35abaccd5b6eface3da9a7a0
JersonVilla <ing.jersonvilla@gmail.com>
2023-10-03 15:35:21 -0500

    Initial commit

---
+++ src/app/clientes/cliente.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { Cliente } from './cliente';
+import { Observable, catchError, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class ClienteService {
+  private urlEndPoint: string = 'http://localhost:8080/api/clientes';
+
+  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getClientes(): Observable<Cliente[]> {
+    //return of(CLIENTES);
+    return this.http.get(this.urlEndPoint).pipe(
+      map(response => response as Cliente[])
+    );
+  }
+
+  getCliente(id:number): Observable<Cliente>{
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
+      catchError(e => {
+        this.router.navigate(['/clientes']);
+        console.error(e.error.mensaje);
+        swal.fire('Error al editar', e.error.mensaje, 'error');
+        return throwError(() => e);
+      })
+    );
+  }
+
+  create(cliente: Cliente) : Observable<Cliente> {
+    return this.http.post(this.urlEndPoint, cliente, {headers: this.httpHeaders}).pipe(
+      map((response: any) => response.cliente as Cliente),
+      catchError(e => {
+
+        /*if (e.status == 400) {
+          return throwError(() => e);
+        }*/
+
+        console.error(e.error.mensaje);
+        swal.fire(e.error.mensaje, e.error.error, 'error');
+        return throwError(() => e);
+      })
+    );
+  }
+
+  update(cliente: Cliente): Observable<any>{
+    return this.http.put<any>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers: this.httpHeaders}).pipe(
+      catchError(e => {
+
+        /*if (e.status == 400) {
+          return throwError(() => e);
+        }*/
+
+        console.error(e.error.mensaje);
+        swal.fire(e.error.mensaje, e.error.error, 'error');
+        return throwError(() => e);
+      })
+    );
+  }
+
+}
